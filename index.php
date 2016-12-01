c17b1200d29cf7614cbe98dc1ae1c323c39eaa7c
Lindsay <brooks.l@zoho.com>
2016-12-01 14:57:46 -0600

    added post links

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 
 <?php get_header(); ?>
-    my awesome theme
+
+<?php
+// the loop
+
+if(have_posts()):
+  while(have_posts()):
+    the_post(); ?>
+  <h2>
+   <a href="<?php the_permalink(); ?>">
+    <?php the_title(); ?>
+   </a>
+  </h2>
+  <?php the_content(); ?>
+<?php endwhile;
+endif;
+?>
+
 <?php get_footer(); ?>
